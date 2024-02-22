a95288d1f0728ee47577aa178fc518f9c4c7d36c
Bahattin Seyhan <bahattin.seyhan@sporveien.com>
2024-02-22 09:47:13 +0100

    modified the config.yml file v35

--- modules/resource-group/outputs.tf
+++ modules/resource-group/outputs.tf
@@ -1,9 +1,9 @@
 output "resource_group_name" {
-  value       = azurerm_resource_group.example.name
+  value       = azurerm_resource_group.terraform_rg.name
   description = "The name of the resource group"
 }
 
 output "resource_group_id" {
-  value       = azurerm_resource_group.example.id
+  value       = azurerm_resource_group.terraform_rg.id
   description = "The ID of the resource group"
 }
